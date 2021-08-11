43d8cbb7119f031fec3b51290aedab81d9a153b9
nickchen <nick45chen@gmail.com>
2021-08-11 23:10:45 +0800

    init

---
+++ lib/mid_level_net.dart
@@ -0,0 +1,14 @@
+part of 'mid_level_network.dart';
+
+class MidLevelNet {
+  MidLevelNet._();
+
+  static late MidLevelNet? _instance;
+
+  static MidLevelNet getInstance() {
+    if (_instance == null) {
+      _instance = MidLevelNet._();
+    }
+    return _instance!;
+  }
+}
---
+++ lib/mid_level_network.dart
@@ -0,0 +1,3 @@
+library mid_level_network;
+
+part 'mid_level_net.dart';
