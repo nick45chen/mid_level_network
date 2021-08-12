761723feb69a053077c916b6c89f94fda04633a6
nickchen <nick45chen@gmail.com>
2021-08-12 15:16:30 +0800

    add test

--- test/mid_level_network_test.dart
+++ test/mid_level_network_test.dart
@@ -1,14 +1,28 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:mid_level_network/mid_level_network.dart';
 
 void main() {
-  // test('adds one to input values', () {
-  //   final calculator = Calculator();
-  //   expect(calculator.addOne(2), 3);
-  //   expect(calculator.addOne(-7), -6);
-  //   expect(calculator.addOne(0), 1);
-  // });
+  test('test network', () async {
+    final MNetRequest mockRequest = MockRequest();
+    final MNetAdapter mockAdapter = MockAdapter();
+    MNetResponse response = await mockAdapter.send(mockRequest);
+    expect(response.statusCode, 200);
+    expect(response.data['data']['message'], 'success');
+  });
+}
+
+class MockRequest extends MNetRequest {
+  @override
+  String authority() => 'example.com';
+
+  @override
+  HttpMethod httpMethod() => HttpMethod.POST;
+
+  @override
+  bool needLogin() => false;
+
+  @override
+  String path() => '/api/user/demo';
 }
 
 class MockAdapter extends MNetAdapter {
